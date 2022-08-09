6e8cafffcc322aa34ac954998ae624e3e780f377
Tobias <t.betz@cortex-media.de>
2022-08-09 15:24:53 +0200

    First clustering steps!

--- reddit_graph/src/js/main.js
+++ reddit_graph/src/js/main.js
@@ -1,41 +1,37 @@
 import Sigma from "sigma";
 import Graph from "graphology";
-import FA2Layout from 'graphology-layout-forceatlas2/worker';
-
+import louvain from 'graphology-communities-louvain';
+import NoverlapLayout from 'graphology-layout-noverlap/worker';
+import chroma from "chroma-js"
 
 fetch('/reddit_sigma.json')
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById("container");
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById("container");
 
-        const graph = new Graph();
+    const graph = new Graph();
 
-        const maxSize = data.nodes.map((node) => node.size).reduce((prev, cur) => prev < cur ? cur : prev)
+    const maxSize = data.nodes.map((node) => node.size).reduce((prev, cur) => prev < cur ? cur : prev)
 
-        data.nodes.forEach(node => graph.addNode(node.id, {label: node.label, size: Math.max(5, (node.size / maxSize) * 40)}))
-        data.edges.forEach(edge => graph.addEdge(edge.source, edge.target))
+    data.nodes.forEach(node => graph.addNode(node.id, { label: node.label, size: Math.max(5, (node.size / maxSize) * 40) }))
+    data.edges.forEach(edge => graph.addEdge(edge.source, edge.target))
 
+    const graphModularity = louvain.detailed(graph)
+    louvain.assign(graph)
 
-        graph.nodes().forEach((node, i) => {
-            const angle = (i * 2 * Math.PI) / graph.order;
-            graph.setNodeAttribute(node, "x", 100 * Math.cos(angle));
-            graph.setNodeAttribute(node, "y", 100 * Math.sin(angle));
-        });
+    const colors = chroma.scale('OrRd').colors(graphModularity.count)
 
-        const renderer = new Sigma(graph, container);
-
-
-        const layout = new FA2Layout(graph, {
-            iterations: 50,
-            settings: {
-                adjustSizes: true,
-                gravity: 10,
-                barnesHutOptimize: true
-            },
-        })
-
-        layout.start()
-
-        setTimeout(() => layout.kill(), 30000)
-
+    graph.nodes().forEach((node, i) => {
+      const angle = (i * 2 * Math.PI) / graph.order;
+      const community = graph.getAttribute(node, "community")
+      graph.setNodeAttribute(node, "color", colors[community])
+      graph.setNodeAttribute(node, "x", graphModularity.communities[node]);
+      graph.setNodeAttribute(node, "y", 100 * Math.sin(angle));
     });
+
+    const renderer = new Sigma(graph, container);
+    // const layout = new NoverlapLayout(graph);
+
+    // layout.start()
+
+  });
